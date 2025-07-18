f4d4d8b8c3e72721d730517facebe7d9abf97742
cristibctr <cristian.bucataru@gmail.com>
2025-07-18 17:14:31 +0300

    almost done

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,13 +1,15 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
 mod macros {
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
+    pub use my_macro;
 }
-
 fn main() {
+    use macros::my_macro;
     my_macro!();
 }
